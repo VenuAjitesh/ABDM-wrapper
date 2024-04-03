2e6e42edeb9c689827e5938988ed26f5c2143e84
VenuAjitesh <85893494+VenuAjitesh@users.noreply.github.com>
2024-04-04 01:34:48 +0530

    Comment on why RestTemplate in HIUClient.

--- src/main/java/com/nha/abdm/wrapper/hip/HIUClient.java
+++ src/main/java/com/nha/abdm/wrapper/hip/HIUClient.java
@@ -12,7 +12,10 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class HIUClient {
   private static final Logger log = LogManager.getLogger(HIUClient.class);
-
+  //The WebClient is throwing 404 not found where as RestTemplate is working //TODO
+  // url: https://dev.abdm.gov.in/api-hiu/data/notification
+  // url: https://dev.abdm.gov.in/patient-hiu/data/notification
+  // These are the known url where WebClient is throwing 404 error.
   public ResponseEntity<GenericResponse> pushHealthInformation(
       String datPushURl, HealthInformationPushRequest healthInformationPushRequest) {
     RestTemplate restTemplate = new RestTemplate();
