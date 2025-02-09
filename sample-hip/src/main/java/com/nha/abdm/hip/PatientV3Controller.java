726050eedfd29199d01327b571996547791087f2
Venu-Ajitesh <Venu@4444>
2025-02-10 02:41:54 +0530

    Added sample-hip in docker config for local testing

--- sample-hip/src/main/java/com/nha/abdm/hip/PatientV3Controller.java
+++ sample-hip/src/main/java/com/nha/abdm/hip/PatientV3Controller.java
@@ -1,25 +1,19 @@
 package com.nha.abdm.hip;
 
-import in.nha.abdm.wrapper.client.api.LinkApi;
-import in.nha.abdm.wrapper.client.api.PatientsApi;
-import in.nha.abdm.wrapper.client.invoker.ApiException;
-import in.nha.abdm.wrapper.client.model.*;
+
+import com.nha.abdm.wrapper.client.model.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Value;
-
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -168,109 +162,6 @@ public class PatientV3Controller {
         return new ResponseEntity<>(healthInformationResponse, HttpStatus.OK);
     }
 
-    @PostMapping({"/test-wrapper/upsert-patients"})
-    public FacadeV3Response upsertPatients() throws ApiException {
-        PatientsApi patientsApi = new PatientsApi();
-
-        List<Patient> patients = new ArrayList<>();
-
-        Patient patient = new Patient();
-        patient.setAbhaAddress("atul_kumar13@sbx");
-        patient.setName("Atul Kumar");
-        patient.setPatientDisplay("Atul");
-        patient.setPatientReference("patient123");
-        patient.setGender(Patient.GenderEnum.M);
-        patient.setPatientMobile("+91-9742181684");
-        patient.setDateOfBirth("1986-10-13");
-
-        patients.add(patient);
-
-        return patientsApi.upsertPatients(patients);
-    }
-
-    @PostMapping({"/test-wrapper/link-carecontexts-demographics"})
-    public FacadeResponse linkCareContextsDemographics() throws ApiException {
-        LinkApi linkApi = new LinkApi();
-
-        CareContext careContext1 = new CareContext();
-        careContext1.setReferenceNumber("care-context-reference31");
-        careContext1.setDisplay("care-context-display31");
-
-        CareContext careContext2 = new CareContext();
-        careContext2.setReferenceNumber("care-context-reference32");
-        careContext2.setDisplay("care-context-display32");
-
-        List<CareContext> careContexts = new ArrayList<>();
-        careContexts.add(careContext1);
-        careContexts.add(careContext2);
-
-        PatientWithCareContext patient = new PatientWithCareContext();
-        patient.setId("atul_kumar13@sbx");
-        patient.setReferenceNumber("patient123");
-        patient.setCareContexts(careContexts);
-
-        LinkCareContextsRequest linkCareContextsRequest = new LinkCareContextsRequest();
-        linkCareContextsRequest.setRequestId(UUID.randomUUID().toString());
-        linkCareContextsRequest.setRequesterId("Demo_Atul_HIP");
-        linkCareContextsRequest.setAbhaAddress("atul_kumar13@sbx");
-
-        return linkApi.linkCareContexts(linkCareContextsRequest);
-    }
-
-    @PostMapping({"/test-wrapper/link-carecontexts-mobile-otp"})
-    public FacadeResponse linkCareContextsMobileOtp() throws  ApiException {
-        LinkApi linkApi = new LinkApi();
-
-        CareContext careContext1 = new CareContext();
-        careContext1.setReferenceNumber("care-context-reference17");
-        careContext1.setDisplay("care-context-display17");
-
-        CareContext careContext2 = new CareContext();
-        careContext2.setReferenceNumber("care-context-reference18");
-        careContext2.setDisplay("care-context-display18");
-
-        List<CareContext> careContexts = new ArrayList<>();
-        careContexts.add(careContext1);
-        careContexts.add(careContext2);
-
-        PatientWithCareContext patient = new PatientWithCareContext();
-        patient.setId("atul_kumar13@sbx");
-        patient.setReferenceNumber("patient123");
-        patient.setCareContexts(careContexts);
-
-        LinkCareContextsRequest linkCareContextsRequest = new LinkCareContextsRequest();
-        linkCareContextsRequest.setRequestId(UUID.randomUUID().toString());
-        linkCareContextsRequest.setRequesterId("Demo_Atul_HIP");
-        linkCareContextsRequest.setAbhaAddress("atul_kumar13@sbx");
-
-        return linkApi.linkCareContexts(linkCareContextsRequest);
-    }
-
-    @PostMapping({"/test-wrapper/verify-otp"})
-    public FacadeResponse verifyOtp(@RequestBody String otp) throws ApiException {
-        LinkApi linkApi = new LinkApi();
-
-        VerifyOTPRequest verifyOTPRequest = new VerifyOTPRequest();
-        verifyOTPRequest.setLoginHint(VerifyOTPRequest.LoginHintEnum.HIPLINKING);
-        verifyOTPRequest.setRequestId(requestId);
-        System.out.println("otp:" + otp);
-        verifyOTPRequest.setAuthCode(otp);
-        return linkApi.verifyOTP(verifyOTPRequest);
-    }
-
-    @GetMapping({"/test-wrapper/link-status"})
-    public String linkStatus() throws ApiException {
-        LinkApi linkApi = new LinkApi();
-
-        // To make this periodic poll, requestId can be persisted to facility's / HIP's database.
-        RequestStatusResponse response = linkApi.linkStatusRequestIdGet(requestId);
-        System.out.println(response.getStatus());
-        if (response.getError() != null) {
-            System.out.println("Error: " + response.getError().getMessage());
-            return response.getError().getMessage();
-        }
-        return  response.getStatus();
-    }
     public static String getRandomFileFromDirectory(String directoryPath) throws IOException {
         try (Stream<Path> paths = Files.list(Paths.get(directoryPath))) {
             List<Path> fileList = paths
